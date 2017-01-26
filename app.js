5946ccd7938c38422794730c630ffc8a17f69a27
NoName64Bits <ciobanulaur@gmail.com>
2017-01-26 18:05:44 +0200

    Added a little more of Echo and some optimizations to ram usage

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const socketIo = require('socket.io');
 
 var app = express();
-var io = socketIo(3000);
 
 const Bouvier = require('./lib/bouvier');
 
@@ -16,6 +15,7 @@ var bouvierOptions = {
   partials: 'partials',
   useables: {
     switchBox: 'switch.bov',
+    repository: 'repo.json',
     layouts: [],
     views: [],
     partials: []
@@ -28,82 +28,13 @@ app.engine('bov', bouvier.render);
 app.set('view engine', 'bov');
 
 app.use(bouvier.process);
-
-app.get('/bouvier/uuid', function(request, response) {
-  response.send("var UUID = '" + request.bouvier.user.id + "';");
-});
+//bouvier.init();
 
 var users = {};
 var count = 0;
 
-io.sockets.on('connection', function(socket){
-  socket.on('bouvier-connect', function(data){
-    if(!users[data.user]){
-      users[data.user] = [];
-    }
-    users[data.user].push(socket.id);
-    console.log(users[data.user]);
-    console.log("User connected: " + data.user + " - " + socket.id);
-  });
 
-  socket.on('bouvier-trigger', function(data){
-    console.log(socket.id + " : " + JSON.stringify(data));
-    findUserBySocket(socket.id);
-    if(data.target == 'increment'){
-      for(var i = 0; i < data.attributes.length; i++){
-        if(data.attributes[i].name == 'count'){
-          count += Number(data.attributes[i].value);
-          updateBySocketID(socket.id, {id: "count", content: count});
-          console.log(count);
-        }
-      }
-    }
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-
-app.get('/socket/:id', function(req, res){
-  for(var i = 0; i < users[req.params.id].length; i++){
-    io.sockets.sockets[users[req.params.id][i]].emit('bouvier-update', {id: "count", content: count});
-  }
-  res.send(typeof io.sockets.sockets[users[req.params.id][0]]);
-});
-
-function updateBySocketID(socket, data){
-  var sockets = findUserBySocket(socket);
-  console.log(sockets);
-  for(var i = 0; i < sockets.length; i++){
-    io.sockets.sockets[sockets[i]].emit('bouvier-update', data);
-  }
-}
-
-function updateByUserID(user, data){
-  for(var i = 0; i < users[user].length; i++){
-    io.sockets.sockets[users[user][i]].emit('bouvier-update', data);
-  }
-}
-
-function findUserBySocket(socket){
-  for(var i = 0; i < Object.keys(users).length; i++){
-    var user = users[Object.keys(users)[i]];
-    if(user.indexOf(socket) >= 0){
-      return user;
-    }
-  }
-}
-
-function findSocketsByUser(user){
-  if(users[user]){
-    return users[user];
-  } else {
-    return [];
-  }
-}
-
-app.get('/:title', function(request, response){
+app.get('/home/:title', function(request, response){
   response.render("home",
     {
       info: request.params.title,
